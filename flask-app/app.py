86313f3b5ae7729f5e6dbc436b3163a8823af97d
MattWong-ca <matthew.yt.wong@gmail.com>
2024-03-29 19:10:35 -0400

    Print cast stream

--- flask-app/app.py
+++ flask-app/app.py
@@ -4,3 +4,8 @@ from dotenv import load_dotenv
 from farcaster import Warpcast
 
 load_dotenv()
+
+client = Warpcast(mnemonic=os.environ.get("MNEMONIC_ENV_VAR"))
+
+for cast in client.stream_casts():
+    print(cast.text)
