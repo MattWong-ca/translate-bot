7b9431c1be73bcd5531a9aff571e055d73686783
MattWong-ca <matthew.yt.wong@gmail.com>
2024-03-30 21:46:10 -0400

    Commit

---
+++ heroku-deploy/.venv/lib/python3.11/site-packages/farcaster/__init__.py
@@ -0,0 +1,20 @@
+"""farcaster-py is a Python SDK for the Farcaster Protocol"""
+
+import sys
+
+from .client import Warpcast  # noqa
+
+if sys.version_info >= (3, 8):
+    from importlib import metadata as importlib_metadata
+else:  # pragma: no cover
+    import importlib_metadata
+
+
+def get_version() -> str:
+    try:
+        return str(importlib_metadata.version(__name__))
+    except importlib_metadata.PackageNotFoundError:  # pragma: no cover
+        return "unknown"
+
+
+version: str = get_version()
