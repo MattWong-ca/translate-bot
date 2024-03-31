904fb3b7ad1ac26f54a6c595d43de1b01ee68840
MattWong-ca <matthew.yt.wong@gmail.com>
2024-03-30 23:33:59 -0400

    Add OpenAI API

--- app.py
+++ app.py
@@ -1,18 +1,20 @@
 import os
 from dotenv import load_dotenv
 from farcaster import Warpcast
+from openai import OpenAI
 
 load_dotenv()
 
-client = Warpcast(mnemonic=os.environ.get("MNEMONIC_ENV_VAR"))
+warpcastClient = Warpcast(mnemonic=os.environ.get("MNEMONIC_ENV_VAR"))
+openaiClient = OpenAI(api_key=os.environ.get("OPENAI_ENV_VAR"))
 
-for cast in client.stream_casts():
+for cast in warpcastClient.stream_casts():
     # CHANGE TO JUST @TRANSLATE LATER
     if cast and "@translate chinese" in cast.text:
         if cast.parent_hash is not None:
-            parentCastText = client.get_cast(cast.parent_hash).cast.text
+            parentCastText = warpcastClient.get_cast(cast.parent_hash).cast.text
             # SEND PARENT CAST TEXT TO OPENAI, GET RESPONSE
-            response = client.post_cast(text="🦉: {}".format(parentCastText), parent={
+            response = warpcastClient.post_cast(text="🦉: {}".format(parentCastText), parent={
                 "fid": 397823,
                 "hash": cast.hash
              })
