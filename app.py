9671b9d340f18a1e722b40fc4f42006604f58d1a
MattWong-ca <matthew.yt.wong@gmail.com>
2024-04-02 19:44:54 -0400

    Update API URL for prod

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import requests
 import json
 
 load_dotenv()
-url = 'http://localhost:3000/api/translate'
+url = 'https://translate-bot-nextjs.vercel.app/api/translate'
 
 warpcastClient = Warpcast(mnemonic=os.environ.get("MNEMONIC_ENV_VAR"))
 openaiClient = OpenAI(api_key=os.environ.get("OPENAI_ENV_VAR"))
@@ -24,8 +24,6 @@ for cast in warpcastClient.stream_casts():
              }) 
     elif cast and "@translate" in cast.text and cast.author.fid != 397823:
         if cast.parent_hash is not None:
-            # SEND USER FID TO DB TO CHECK IF IT'S OVER 10 USES
-            # fid = {'number': cast.author.fid}
             parentCastText = warpcastClient.get_cast(cast.parent_hash).cast.text
             completion = openaiClient.chat.completions.create(
                 model="gpt-3.5-turbo",
